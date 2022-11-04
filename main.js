edb8cbb7a7e1a6c78ef801876848d01d107edb07
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-04 21:22:09 +0000

    Added functions

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var $form = document.querySelector('form');
+var $overlay = document.querySelector('.overlay');
+var $entryBtn = document.querySelector('.entry-btn');
+
+$entryBtn.addEventListener('click', addEntry);
 
 $form.addEventListener('submit', submitEntry);
 
-function submitEntry(event) {}
+function submitEntry(event) {
+  event.preventDefault();
+  $overlay.className = 'overlay positioned hidden';
+}
+
+function addEntry(event) {
+  $overlay.className = 'overlay positioned';
+}
